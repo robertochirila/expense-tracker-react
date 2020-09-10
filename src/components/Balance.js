c06e443628b936658fe90c2ba25805b439579d0e
Roberto <robertochirila@yahoo.com>
2020-09-10 14:56:40 +0100

    added calculations

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
   return (
     <>
       <h4>Balance</h4>
